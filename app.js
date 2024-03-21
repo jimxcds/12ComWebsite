2c0a7559936a42b40cff2c720bd64728f4fe2f37
jimxcds <95667665+jimxcds@users.noreply.github.com>
2024-03-21 22:24:56 +1300

    Added More Animations

--- app.js
+++ app.js
@@ -6,30 +6,44 @@ function showMoblieNav() {
         mobileNav.style.display = "block";
     }
 }
-
-let target = document.querySelector("#firstTextBox");
-
-let options = {
-  // null is to target document viewport
-  root: null,
-  // bounding box of target element before computing visibility
-  rootMargin: "0px",
-  // indicates at what percentage of the target's visibility the ovbserver callback should be executed
-  threshold: [0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
-}
-
-let callback = (entries, observer) => {
+// let target = document.querySelector("#firstTextBox");
+const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry.intersectionRatio);
-    if (entry.intersectionRatio>.99){
-        
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('textBoxAnimation');
+    }else {
+      //entry.target.classList.remove('textBoxAnimation');
     }
-    entry.target.style.opacity = entry.intersectionRatio;
-  })
+  });
+}, {
+  threshold: 0.2
+});
+
+const quoteText = document.querySelectorAll('.quoteText');
+quoteText.forEach((el) => observer.observe(el));
+
+// let options = {
+//   // null is to target document viewport
+//   root: null,
+//   // bounding box of target element before computing visibility
+//   rootMargin: "0px",
+//   // indicates at what percentage of the target's visibility the ovbserver callback should be executed
+//   threshold: [0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1]
+// }
+
+// let callback = (entries, observer) => {
+//   entries.forEach((entry) => {
+//     console.log(entry.intersectionRatio);
+//     if (entry.intersectionRatio>.99){
+        
+//     }
+//     //entry.target.style.opacity = entry.intersectionRatio;
+//   })
   
-}
+// }
 
-// create the intersection observer
-let observer = new IntersectionObserver(callback, options);
+// // create the intersection observer
+// let observer = new IntersectionObserver(callback, options);
 
-observer.observe(target);
+// observer.observe(target);
